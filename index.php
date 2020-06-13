8e60fe20632f003b33210bd2b4f6e23758ee5ca3
adimael <adimaelbr@gmail.com>
2020-06-13 11:10:22 -0300

    Criado pageAdmin e tpl admin

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use Fincorp\Page;
+use \Fincorp\Page;
+use \Fincorp\PageAdmin;
 
 $app = new Slim();
 
@@ -14,6 +15,14 @@ $app->get('/', function() {
 		$page = new Page();
 
 		$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
 
 });
 
