dd7b112259c58cde7e98193a7d4b571a16829b65
adimael <adimaelbr@gmail.com>
2020-06-17 10:05:54 -0300

    Site - Produtos

--- cache/index.bb187e8c4656f07e429a12aabf2ef1b6.rtpl.php
+++ cache/index.bb187e8c4656f07e429a12aabf2ef1b6.rtpl.php
@@ -1,47 +1,47 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?><div class="slider-area">
-  <!-- Slider -->
-<div class="block-slider block-slider4">
-<ul class="" id="bxslider-home4">
-  <li>
-    <img src="/res/page/img/h4-slide.png" alt="Slide">
-    <div class="caption-group">
-      <h2 class="caption title">
-        iPhone <span class="primary">6 <strong>Plus</strong></span>
-      </h2>
-      <h4 class="caption subtitle">Dual SIM</h4>
-      <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+    <!-- Slider -->
+    <div class="block-slider block-slider4">
+        <ul class="" id="bxslider-home4">
+            <li>
+                <img src="/res/page/img/h4-slide.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        iPhone <span class="primary">6 <strong>Plus</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">Dual SIM</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/page/img/h4-slide2.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        by one, get one <span class="primary">50% <strong>off</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">school supplies & backpacks.*</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/page/img/h4-slide3.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        Apple <span class="primary">Store <strong>Ipod</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">Select Item</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/page/img/h4-slide4.png" alt="Slide">
+                <div class="caption-group">
+                  <h2 class="caption title">
+                        Apple <span class="primary">Store <strong>Ipod</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">& Phone</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+        </ul>
     </div>
-  </li>
-  <li><img src="/res/page/img/h4-slide2.png" alt="Slide">
-    <div class="caption-group">
-      <h2 class="caption title">
-        by one, get one <span class="primary">50% <strong>off</strong></span>
-      </h2>
-      <h4 class="caption subtitle">school supplies & backpacks.*</h4>
-      <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
-    </div>
-  </li>
-  <li><img src="/res/page/img/h4-slide3.png" alt="Slide">
-    <div class="caption-group">
-      <h2 class="caption title">
-        Apple <span class="primary">Store <strong>Ipod</strong></span>
-      </h2>
-      <h4 class="caption subtitle">Select Item</h4>
-      <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
-    </div>
-  </li>
-  <li><img src="/res/page/img/h4-slide4.png" alt="Slide">
-    <div class="caption-group">
-      <h2 class="caption title">
-        Apple <span class="primary">Store <strong>Ipod</strong></span>
-      </h2>
-      <h4 class="caption subtitle">& Phone</h4>
-      <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
-    </div>
-  </li>
-</ul>
-</div>
-<!-- ./Slider -->
+    <!-- ./Slider -->
 </div> <!-- End slider area -->
 
 <div class="promo-area">
@@ -84,95 +84,25 @@
             <div class="latest-product">
                 <h2 class="section-title">Produtos</h2>
                 <div class="product-carousel">
+                    <?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
+
                     <div class="single-product">
                         <div class="product-f-image">
-                            <img src="/res/page/img/product-1.jpg" alt="">
+                            <img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
                             <div class="product-hover">
-                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
-                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                <a href="/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Comprar</a>
+                                <a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i> Ver Detalhes</a>
                             </div>
                         </div>
                         
-                        <h2><a href="#">Samsung Galaxy s5- 2015</a></h2>
+                        <h2><a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
                         
                         <div class="product-carousel-price">
-                            <ins>$700.00</ins> <del>$100.00</del>
+                            <ins>R$<?php echo formatPrice($value1["vlprice"]); ?></ins>
                         </div> 
                     </div>
-                    <div class="single-product">
-                        <div class="product-f-image">
-                            <img src="/res/page/img/product-2.jpg" alt="">
-                            <div class="product-hover">
-                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
-                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
-                            </div>
-                        </div>
-                        
-                        <h2>Nokia Lumia 1320</h2>
-                        <div class="product-carousel-price">
-                            <ins>$899.00</ins> <del>$999.00</del>
-                        </div> 
-                    </div>
-                    <div class="single-product">
-                        <div class="product-f-image">
-                            <img src="/res/page/img/product-3.jpg" alt="">
-                            <div class="product-hover">
-                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
-                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
-                            </div>
-                        </div>
-                        
-                        <h2>LG Leon 2015</h2>
+                    <?php } ?>
 
-                        <div class="product-carousel-price">
-                            <ins>$400.00</ins> <del>$425.00</del>
-                        </div>                                 
-                    </div>
-                    <div class="single-product">
-                        <div class="product-f-image">
-                            <img src="/res/page/img/product-4.jpg" alt="">
-                            <div class="product-hover">
-                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
-                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
-                            </div>
-                        </div>
-                        
-                        <h2><a href="#">Sony microsoft</a></h2>
-
-                        <div class="product-carousel-price">
-                            <ins>$200.00</ins> <del>$225.00</del>
-                        </div>                            
-                    </div>
-                    <div class="single-product">
-                        <div class="product-f-image">
-                            <img src="/res/page/img/product-5.jpg" alt="">
-                            <div class="product-hover">
-                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
-                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
-                            </div>
-                        </div>
-                        
-                        <h2>iPhone 6</h2>
-
-                        <div class="product-carousel-price">
-                            <ins>$1200.00</ins> <del>$1355.00</del>
-                        </div>                                 
-                    </div>
-                    <div class="single-product">
-                        <div class="product-f-image">
-                            <img src="/res/page/img/product-6.jpg" alt="">
-                            <div class="product-hover">
-                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
-                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> See details</a>
-                            </div>
-                        </div>
-                        
-                        <h2><a href="#">Samsung gallaxy note 4</a></h2>
-
-                        <div class="product-carousel-price">
-                            <ins>$400.00</ins>
-                        </div>                            
-                    </div>
                 </div>
             </div>
         </div>
