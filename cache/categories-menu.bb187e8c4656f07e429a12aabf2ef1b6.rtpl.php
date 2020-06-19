bcb400a611114424c71839dadd70784045585968
adimael <adimaelbr@gmail.com>
2020-06-19 10:39:39 -0300

    Limpeza do cache

--- cache/categories-menu.bb187e8c4656f07e429a12aabf2ef1b6.rtpl.php
+++
@@ -1,1 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/4">Android</a></li><li><a href="/categories/5">Apple</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/8">Produtos disponíveis</a></li><li><a href="/categories/7">Samsung</a></li>
